fe4c05bc7acb65381adebcf7faa88d3c3a7f4b3d
moi <moi>
2023-01-24 19:07:15 +0100

    remove non used packages

--- appfile.py
+++ appfile.py
@@ -1,11 +1,6 @@
-
-import pandas as pd
-import numpy as np
 import pickle
 from fastapi import FastAPI
 import uvicorn
-import gunicorn
-from typing import List
 from pydantic import BaseModel
 from sklego.meta import Thresholder
 
